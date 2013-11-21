53b6f23b0cfc997500602f14064c6d7d73c1cbdc
joeturing <jtaldan@gmail.com>
2013-11-21 13:16:55 -0600

    datos de google maps
    se agregaron los datos de la direccion en google maps.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
     Google maps
 */
 jQuery(document).ready(function() {
-    var position = new google.maps.LatLng(45.067883, 7.687231);
+    var position = new google.maps.LatLng(30.068184, -95.481877);
     $('.map').gmap({'center': position,'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
             var self = this;
             self.addMarker({'position': this.get('map').getCenter() });	
